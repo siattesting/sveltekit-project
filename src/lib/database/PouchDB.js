9537f4109ff9134260ce4557d8f343a5c0aab3d9
siat.testing <siat.testing@gmail.com>
2023-09-01 15:00:43 +0000

    Initial Commit

--- src/lib/database/PouchDB.js
+++ src/lib/database/PouchDB.js
@@ -22,3 +22,27 @@ export async function deleteItem(item) {
 	const result = await DB.remove(item);
 	return result;
 }
+
+/**
+ * @param {{ title: string; city: string; created_at: string; updated_at: string; type: string; _id: string; }} item
+ */
+export async function createPartner(item) {
+	if (item.title === '') {
+		throw new Error('You must provide a title');
+	}
+	if (item.city === '') {
+		throw new Error('You must provide a City');
+	}
+	item.created_at = new Date().toISOString();
+	item.updated_at = new Date().toISOString();
+	item.type = 'partner';
+	item._id = 'partner:' + crypto.randomUUID();
+	const result = await DB.post(item);
+	return result;
+}
+
+export async function getPartners() {
+	const { rows } = await DB.allDocs({ include_docs: true})
+	const results = rows.filter((row) => row.doc.type === 'partner')
+	const results.map(({ doc }) => doc)
+}
---
+++ src/routes/partners/+page.server.js
@@ -0,0 +1,40 @@
+import * as DB from '$lib/database/PouchDB.js';
+import { fail } from '@sveltejs/kit';
+
+export async function load() {
+	// const docs = await DB.listAllItems()
+	const partners = await DB.getPartners();
+	return {
+		partnerlist: partners.map((partner) => ({
+			id: partner._id,
+			rev: partner._rev,
+			title: partner.title,
+			city: partner.city,
+			type: partner.type,
+			created_at: partner.created_at,
+			updated_at: partner.updated_at
+		}))
+	};
+}
+
+export const actions = {
+	create: async ({ request }) => {
+		const data = await request.fromData();
+		const title = data.get('title');
+		const city = data.get('city');
+
+		if (!title || !city) {
+			throw new Error();
+		}
+
+		try {
+			DB;
+		} catch (error) {
+			return fail(422, {
+				title: data.get('title'),
+				city: data.get('city'),
+				error: error.message
+			});
+		}
+	}
+};
